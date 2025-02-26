0051680dc1dff82d050ff5229fa5fa606b645661
Tejas-1323 <tejaswagh0111@gmail.com>
2025-02-26 13:48:26 +0530

    update  a middeleware

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require("express");
+const app = express();
+const port = 4000;
 
-app.use(express.json())
+app.use(express.json());
 
-const middelWare = (req, res, next) => {
- try {
+const middleware = (req, res, next) => {
+  try {
+    if (!req.body || Object.keys(req.body).length === 0) {
+      return res
+        .status(400)
+        .send({ message: "Empty request body is not allowed" });
+    } else {
+        console.log(`Request Method: ${req.method}, URL: ${req.url}`);
+        next();
+    }
 
-    let data = `${req.method} + ${req.url}`;
-    console.log("req method is : ", data);
-    next();
- } catch (error) {
-   console.log(error);
- }
+
+  } catch (error) {
+    console.error("Middleware Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
-app.use(middelWare);
+app.use(middleware);
 
-app.get('/', (req,res) => {
-  res.send({message:"hello i am working now"})
-})
+app.get("/", (req, res) => {
+  res.send({ message: "hello i am working now" });
+});
 
-app.post('/sum', (req, res) => {
-  const { a, b } = req.body
+app.post("/sum", (req, res) => {
+  const { a, b } = req.body;
   let result = a + b;
-  res.send({message:"sum of two no is",result})
-})
+  res.send({ message: "sum of two no is", result });
+});
 
-
-
-
-app.listen(port,() => {
+app.listen(port, () => {
   console.log(`server is running on ${port}`);
-
-})
+});
