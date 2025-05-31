9c0698ebda07d29e9a60e00a9f6abb1a1e9d3c70
Tejas-1323 <tejaswagh0111@gmail.com>
2025-05-31 11:27:42 +0530

    practes in may month

--- May_1.js
+++ May_1.js
@@ -191,3 +191,108 @@ console.log("hello do somthing with you !!!!")
 // }
 
 // console.log(x(10)(10))
+
+
+// const person = {
+//   name: "tejas",
+//   data: function () {
+//   console.log(`my name is ${this.name}`);
+
+//   }
+// }
+
+
+// person.data()
+
+
+// const array = ['tejas', 'jayesh', 'lalit', 'suraj', 'deepak', 'ashraf']
+// console.log(array.length);
+// console.log(array[3]);
+
+// const data = array.slice(2,5)
+// console.log(data)
+
+
+// call applay bind
+
+// const person ={name:'tej'}
+
+// function hello(city) {
+//   console.log(`my name is ${this.name} and i am from ${city}`);
+
+// }
+
+// hello.call(person, 'pune')
+// hello.apply(person, ['chopda']);
+
+// const newdata = hello.bind(person,'nashik')
+// newdata()
+
+
+// map filter reduse foreach
+
+
+// const array = [1, 2, 3, 4]
+
+// const data = array.map(num => num * 2)
+// console.log(data);
+
+// const find = array.filter(num => num % 2 == 0)
+// console.log(find);
+
+// const value = array.reduce((acc, num) => acc + num, 0)
+// console.log(value);
+
+//  array.forEach(num => console.log(num*2))
+
+
+// shallo copy and deep copy
+
+
+// const obj = {
+//   name: 'tejas',
+//   detailes: {
+//     age:38
+//   }
+// }
+
+// const obj2 = { ...obj }
+// obj2.detailes.age = 55
+// console.log(obj2.detailes.age);
+// console.log(obj.detailes.age);
+// console.log(obj2);
+
+
+// let obj2 = JSON.parse(JSON.stringify(obj))
+// obj2.detailes.age = 66
+// console.log(obj2.detailes.age);
+// console.log(obj.detailes.age);
+
+
+// console.log(obj2);
+// console.log(obj);
+
+
+
+// // Creating a Buffer from a string
+// const buffer = Buffer.from("Hello, World!", "utf-8");
+// console.log(buffer);
+
+// // Converting Buffer to JSON object
+// const json = buffer.toJSON();
+// console.log(json); // { type: 'Buffer', data: [ 72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33 ] }
+
+// // Converting Buffer to String
+// const text = buffer.toString("utf-8");
+// console.log(text); // Hello, World!
+
+
+// import EventEmitter from "events";
+// // Create an instance of EventEmitter
+// const myEmitter = new EventEmitter();
+// // Add a listener for the 'greet' event
+// myEmitter.on("greet", () => {
+//   console.log("Hello, world!");
+// });
+// // Emit the 'greet' event
+// myEmitter.emit("greet");
